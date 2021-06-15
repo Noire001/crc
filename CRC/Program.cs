cc13e1ec77b9bfcea20145afdcf56559be408e56
Konstantinos Karakostas <konstantk@csd.auth.gr>
2021-06-15 15:22:36 +0300

    add uint to array conversion, random message & noise generator

--- CRC/Program.cs
+++ CRC/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CRC
 {
@@ -9,13 +10,52 @@ namespace CRC
             Console.WriteLine("Enter k:");
             uint k = Convert.ToUInt32(Console.ReadLine());
             Console.WriteLine("Enter P:");
-            uint P = Convert.ToUInt32(Console.ReadLine());
+            uint pNum = Convert.ToUInt32(Console.ReadLine());
+            uint[] P = DigitsToArray(pNum);
             Console.WriteLine("Enter BER:");
-            uint BER = Convert.ToUInt32(Console.ReadLine());
-
-            uint[] block = new uint[k];
+            double BER = Convert.ToDouble(Console.ReadLine());
+            
+            uint[] block = RandomMessage(k, P.Length - 1);
+            
         }
-        
-        
+
+        static uint[] DigitsToArray(uint digits)
+        {
+            if (digits == 0) return new uint[] { 0 };
+            var dList = new List<uint>();
+            for (; digits != 0; digits /= 10)
+                dList.Add(digits % 10);
+            var arr = dList.ToArray();
+            Array.Reverse(arr);
+            return arr;
+        }
+
+        static uint[] RandomMessage(uint k, int slide)
+        {
+            var block = new uint[k + slide];
+            var random = new Random();
+            for (int i = 0; i < block.Length - slide; i++)
+            {
+                block[i] = Convert.ToUInt32(random.Next(0, 2));
+            }
+            return block;
+        }
+
+        static uint[] MessageWithNoise(uint[] message, double BER)
+        {
+            var random = new Random();
+            for (int i = 0; i < message.Length; i++)
+            {
+                if (random.NextDouble() < BER)
+                {
+                    if (message[i] == 1)
+                        message[i] = 0;
+                    else
+                        message[i] = 1;
+                }
+            }
+
+            return message;
+        }
     }
 }
