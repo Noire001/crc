505d3c4e63e1a6fd7918450c1a979018759a323f
Konstantinos Karakostas <konstantk@csd.auth.gr>
2021-06-15 13:17:22 +0300

    initial commit

---
+++ CRC/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CRC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter k:");
+            uint k = Convert.ToUInt32(Console.ReadLine());
+            Console.WriteLine("Enter P:");
+            uint P = Convert.ToUInt32(Console.ReadLine());
+            Console.WriteLine("Enter BER:");
+            uint BER = Convert.ToUInt32(Console.ReadLine());
+
+            uint[] block = new uint[k];
+        }
+        
+        
+    }
+}
